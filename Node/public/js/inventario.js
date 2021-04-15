1f289fe652a7c22e60ed5e3b1117004841042bf0
tzn <hola@hola.com>
2021-04-15 02:36:26 +0200

    cambios en carrito + estructura tests

--- Node/public/js/inventario.js
+++ Node/public/js/inventario.js
@@ -1,27 +1,27 @@
 
 
 /* ---------  Inicializar valores ---------------------*/
-let totalCarrito=0;
+let totalCarrito = 0;
 let productos = [];
 
 let hamburguesa = new Object({
 	nombre: "hamburguesa",
 	cantidad: 2,
-	precio:6,
+	precio: 6,
 	desc: "Lechuga, tomate y queso"
 });
 
 let pizza = new Object({
 	nombre: "pizza",
 	cantidad: 10,
-	precio:18,
+	precio: 18,
 	desc: "Mozzarela, tomate y jamón"
 });
 
 let menu = new Object({
 	nombre: "menu",
 	cantidad: 10,
-	precio:15,
+	precio: 15,
 	desc: "Menú de hamburguesa"
 });
 
@@ -29,7 +29,7 @@ let menu = new Object({
 let menuDia = new Object({
 	nombre: "menú del día",
 	cantidad: 20,
-	precio:18,
+	precio: 18,
 	desc: "Primero, segundo, postre"
 });
 
@@ -37,7 +37,7 @@ let menuDia = new Object({
 let casera = new Object({
 	nombre: "comida casera",
 	cantidad: 5,
-	precio:20,
+	precio: 20,
 	desc: "consultar"
 });
 
@@ -45,7 +45,7 @@ let casera = new Object({
 let ensalada = new Object({
 	nombre: "ensalada",
 	cantidad: 5,
-	precio:12,
+	precio: 12,
 	desc: "ensalada mixta"
 });
 
@@ -87,62 +87,76 @@ let tortilla = new Object({
 */
 /* Añadir producto ------------------------------------------- */
 
-  function addProduct(product) {
-	  
-	if (product.cantidad>0){
+function addProduct(product) {
 
-		if(localStorage.getItem('compra')==undefined){
+	if (product.cantidad > 0) {
+
+		if (localStorage.getItem('compra') == undefined) {
 			localStorage.setItem('compra', JSON.stringify(new Array()));
 		}
 
 		product.cantidad--;
-		
-		let arrayCompras=JSON.parse(localStorage.getItem('compra'));
+
+		let arrayCompras = JSON.parse(localStorage.getItem('compra'));
 		arrayCompras.push(product);
-		localStorage.setItem('compra', JSON.stringify(arrayCompras));		
+		localStorage.setItem('compra', JSON.stringify(arrayCompras));
 
-		totalCarrito = totalCarrito + product.precio; 
+		totalCarrito = totalCarrito + product.precio;
 
-  		recalcularTotales(product);
+		recalcularTotales(product);
 		console.log('añado producto al carrito');
 		refrescarCabecera();
-  }else{
-	  alert("No quedan unidades en stock");
-  }
+	} else {
+		alert("No quedan unidades en stock");
+	}
 }
 
-function borrar (){
-	localStorage.clear();
+function borrar() {
+	//localStorage.clear();
+	localStorage.removeItem('compra');
+	localStorage.removeItem('monto');
 	refrescarCarrito();
 }
 
-function comprar(){
-	if(estaLogged()){
-		alert("¡Compra realizada con éxito!");
-		borrar();
-	}else {
-			alert("Debes iniciar sesión para realizar la compra");
+function comprar() {
+	if (estaLogged()) {
+		let comproArrayCompras = JSON.parse(localStorage.getItem('compra'));
+		for (var i = 0; i < comproArrayCompras.length; i++) {
+			if (comproArrayCompras[i].cantidad = 0) {
+				alert("hay un producto fuera de stock, va a ser eliminado del carrito.");
+				localStorage.removeItem('compra');
+				comproArrayCompras.splice(i,1);
+				localStorage.setItem('compra', JSON.stringify(comproArrayCompras));
+			}
+		}
+		if (JSON.parse(localStorage.getItem("compra"))!= undefined) {
+			alert("¡Compra realizada con éxito!");
+			borrar();
+		}
+	} else {
+		alert("Debes iniciar sesión para realizar la compra");
 	}
 
 }
 
 // Calculos 
 
-function refrescarCabecera(){
-	if(JSON.parse(localStorage.getItem("compra"))!=null){
-	var longitud = JSON.parse(localStorage.getItem("compra"));
-   document.getElementById('numTotalProductos').innerHTML = longitud.length;}
+function refrescarCabecera() {
+	if (JSON.parse(localStorage.getItem("compra")) != null) {
+		var longitud = JSON.parse(localStorage.getItem("compra"));
+		document.getElementById('numTotalProductos').innerHTML = longitud.length;
+	}
 }
 
 
-function recalcularTotales (){
+function recalcularTotales() {
 	localStorage.setItem('monto', totalCarrito);
 
 }
-function estaLogged(){
-	if (localStorage.getItem('usuario')==undefined){
+function estaLogged() {
+	if (localStorage.getItem('usuario') == undefined) {
 		return false;
-	}else {
+	} else {
 		return true;
 	}
 }
@@ -154,7 +168,6 @@ module.exports = {
 	borrar : borrar,
 	comprar : comprar,
 	refrescarCabecera : refrescarCabecera,
-	refrescarCarrito : refrescarCarrito,
 	recalcularTotales : recalcularTotales,
-	crearCesta : crearCesta
+	estaLogged : estaLogged
 }*/
--- Node/public/js/usuarios.js
+++ Node/public/js/usuarios.js
@@ -1,59 +1,59 @@
 // ----- LogIn --------------------
-function login(){
+function login() {
     let user = document.getElementById("inputUser").value;
     let pass = document.getElementById("inputPass").value;
- 
-    $.getJSON("../jsons/login.json", function(json) { 
-     let arrayUsers = json;
-     console.log(JSON.stringify(arrayUsers));
- 
-    for (let i = 0; i<arrayUsers.length; i++){
-     if(arrayUsers[i].nombre===user && arrayUsers[i].pass===pass){
-         localStorage.setItem('usuario', JSON.stringify(arrayUsers[i]));
-         console.log("Usuario logeado");
-      }
-      }
- 
-     if(!isLogged()){
-         let arrayRegistrados=JSON.parse(localStorage.getItem('registrados'));
-         for (let i = 0; i<arrayRegistrados.length; i++){
-             if(arrayRegistrados[i].nombre===user && arrayRegistrados[i].pass===pass){
-                 localStorage.setItem('usuario', JSON.stringify(arrayRegistrados[i]));
-                 console.log("Usuario logeado");
-              }
-              }
-     }
- 
-     });
 
-     refrescarLogin();
- }
- 
- function isLogged(){
-     if (localStorage.getItem('usuario')==undefined){
-         return false;
-     }else {
-         return true;
-     }
- }
+    $.getJSON("../jsons/login.json", function (json) {
+        let arrayUsers = json;
+        console.log(JSON.stringify(arrayUsers));
 
- // ----- LogIn --------------------
- function logout(){
+        for (let i = 0; i < arrayUsers.length; i++) {
+            if (arrayUsers[i].nombre === user && arrayUsers[i].pass === pass) {
+                localStorage.setItem('usuario', JSON.stringify(arrayUsers[i]));
+                console.log("Usuario logeado");
+            }
+        }
+
+        if (!isLogged()) {
+            let arrayRegistrados = JSON.parse(localStorage.getItem('registrados'));
+            for (let i = 0; i < arrayRegistrados.length; i++) {
+                if (arrayRegistrados[i].nombre === user && arrayRegistrados[i].pass === pass) {
+                    localStorage.setItem('usuario', JSON.stringify(arrayRegistrados[i]));
+                    console.log("Usuario logeado");
+                }
+            }
+        }
+
+    });
+
+    refrescarLogin();
+}
+
+function isLogged() {
+    if (localStorage.getItem('usuario') == undefined) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// ----- LogIn --------------------
+function logout() {
     localStorage.clear();
     refrescarLogin();
- }
+}
 
- // Register
+// Register
 
- function registrarse() {
+function registrarse() {
     let name = $("#inputUser").val();
     let pass = $("#inputPass").val();
 
-    if(localStorage.getItem('registrados')==undefined){
+    if (localStorage.getItem('registrados') == undefined) {
         localStorage.setItem('registrados', JSON.stringify(new Array()));
     }
 
-    let arrayRegistrados=JSON.parse(localStorage.getItem('registrados'));
+    let arrayRegistrados = JSON.parse(localStorage.getItem('registrados'));
 
     let user = new Object();
     user.nombre = name;
@@ -67,7 +67,7 @@ function login(){
 
 //Actualizar
 
-function refrescarLogin(){
+function refrescarLogin() {
     let loginForm = document.getElementById("loginForm");
     let logedForm = document.getElementById("loginRealizado");
     let registerForm = document.getElementById("registerForm");
@@ -76,30 +76,30 @@ function refrescarLogin(){
     if (isLogged()) {
         ocultarAtributo(loginForm);
         mostrarAtributo(logedForm);
-        document.getElementById("botonLogIn").innerHTML="Cerrar Sesión";
+        document.getElementById("botonLogIn").innerHTML = "Cerrar Sesión";
         ocultarAtributo(botonRegistrarse);
         ocultarAtributo(registerForm);
     } else {
         mostrarAtributo(loginForm);
         ocultarAtributo(logedForm);
-        document.getElementById("botonLogIn").innerHTML="Iniciar Sesión";
+        document.getElementById("botonLogIn").innerHTML = "Iniciar Sesión";
         mostrarAtributo(registerForm);
     }
 }
 
-function ocultarAtributo(ide){
-    if(ide){
-        ide.setAttribute("hidden",true);
+function ocultarAtributo(ide) {
+    if (ide) {
+        ide.setAttribute("hidden", true);
     }
 }
-function mostrarAtributo(ide){
-    if(ide){
+function mostrarAtributo(ide) {
+    if (ide) {
         ide.removeAttribute("hidden");
     }
 }
 /*
 module.exports = {
-	loin : login,
+    loin : login,
     isLogged : isLogged,
     registrarse : registrarse,
     refrescarLogin : refrescarLogin
